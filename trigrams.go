f4e763218ef443ae6ef87c985d1f7538b8d22aa4
Sergey <s@unixowl.com>
2019-01-19 03:54:43 +0300

    optimize count function

--- trigrams.go
+++ trigrams.go
@@ -49,8 +49,7 @@ func getTrigramsWithPositions(text string) map[string]int {
 func count(text string) map[string]int {
 	var r1, r2, r3 rune
 	trigrams := map[string]int{}
-	var txt []rune
-
+	txt := make([]rune, 0, len(text)+1)
 	for _, r := range text {
 		txt = append(txt, unicode.ToLower(toTrigramChar(r)))
 	}
@@ -61,15 +60,7 @@ func count(text string) map[string]int {
 	for i := 1; i < len(txt); i++ {
 		r3 = txt[i]
 		if !(r2 == ' ' && (r1 == ' ' || r3 == ' ')) {
-			trigram := []rune{}
-			trigram = append(trigram, r1)
-			trigram = append(trigram, r2)
-			trigram = append(trigram, r3)
-			if trigrams[string(trigram)] == 0 {
-				trigrams[string(trigram)] = 1
-			} else {
-				trigrams[string(trigram)]++
-			}
+			trigrams[string([]rune{r1, r2, r3})]++
 		}
 		r1 = r2
 		r2 = r3
